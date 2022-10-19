2a7c6efb46c3f4dc3b6e6af13d421f0c6c935299
Pocoyo-hue <nicolashermozavillavicencio@gmail.com>
2022-10-18 20:59:38 -0500

    v3.5

--- py/backend.py
+++ py/backend.py
@@ -61,8 +61,8 @@ class database:
         except Exception as e:
             raise
 
-    def insert_user(self, username, password, rol):
-        sql="insert into tbl_users (user_name,user_password,id_rol,estado) values(%s,%s,%s,%s);"
+    def insert_user(self, username, password, rol,nombre,apellido):
+        sql="insert into tbl_users (user_name,user_password,id_rol,estado,nombre,apellido) values(%s,%s,%s,%s,%s,%s);"
         try:
             self.cursor.execute(sql,(username,password,rol,True))
             self.connection.commit()
@@ -71,9 +71,21 @@ class database:
         except Exception as e:
             raise
     #funciones para consultas y insert en la tabla 'tbl_ropa'
+    def select_browser(self,text):
+        sql='select tbl_ropa.id as id, tbl_talla.talla as talla, nombre_ropa from tbl_ropa inner join tbl_talla on tbl_ropa.id_talla = tbl_talla.id where nombre_ropa like "%{}%"'.format(text)
+        try:
+            self.cursor.execute(sql)
+            columns = [column[0] for column in self.cursor.description]
+            results = []
+            for row in self.cursor.fetchall():
+                results.append(dict(zip(columns, row)))
+            return results
+        except Exception as e:
+            raise
+
     def select_ropa(self,id):
 
-        sql= 'select tbl_ropa.id,nombre_ropa,tbl_talla.talla as talla from tbl_ropa inner join tbl_talla on tbl_ropa.id_talla = tbl_talla.id where id={}'.format(id) #hace un select con el id
+        sql= 'select tbl_ropa.id,nombre_ropa,tbl_talla.talla as talla from tbl_ropa inner join tbl_talla on tbl_ropa.id_talla = tbl_talla.id where tbl_ropa.id={}'.format(id) #hace un select con el id
 
         try:
             self.cursor.execute(sql)
@@ -101,7 +113,7 @@ class database:
             raise
     
     def update_ropa(self,id,cantidad):
-        sql= "update tbl_ropa set cantidad=cantidad-{} where id={}".format(cantidad,id)
+        sql= "update tbl_ropa set cantidad=cantidad-{} where tbl_ropa.id={}".format(cantidad,id)
 
         try:
             self.cursor.execute(sql)
@@ -109,8 +121,8 @@ class database:
         except Exception as e:
             raise
 
-    def insert_ropa(self,nombre,cantidad):
-        sql="insert into tbl_ropa (nombre_ropa,cantidad) values('{}',{});".format(nombre,cantidad)
+    def insert_ropa(self,nombre,cantidad,talla):
+        sql="insert into tbl_ropa (nombre_ropa,cantidad,id_talla) values('{}',{},{});".format(nombre,cantidad,talla)
         try:
             self.cursor.execute(sql)
             self.connection.commit()
@@ -119,8 +131,8 @@ class database:
         except Exception as e:
             raise
     #select y insert en la tabla 'tbl_venta'
-    def insert_venta(self,fecha,hora,total):
-        sql="insert into tbl_venta (fecha,hora,total) values('{}','{}',{})".format(fecha,hora,total)
+    def insert_venta(self,fecha,hora,total,u,u_c):
+        sql="insert into tbl_venta (fecha,hora,total,id_user,id_user_caja) values('{}','{}',{},{},{})".format(fecha,hora,total,u,u_c)
         try:
             self.cursor.execute(sql)
             self.connection.commit()
@@ -130,7 +142,7 @@ class database:
             raise
     
     def select_venta(self,id):
-        sql="select id,date_format(fecha, '%d-%m-%Y') as fecha,date_format(hora, '%H:%i:%s') as hora from tbl_venta where id={}".format(id)
+        sql="select id,date_format(fecha, '%d-%m-%Y') as fecha,date_format(hora, '%H:%i:%s') as hora, total, tbl_user.nombre, tbl_user.apellido from tbl_venta inner join tbl_user on tbl_venta.id_user = tbl_user.id where id={}".format(id)
 
         try:
             self.cursor.execute(sql)
@@ -143,7 +155,7 @@ class database:
             raise
     
     def select_venta_all(self):
-        sql="select id,date_format(fecha, '%d-%m-%Y') as fecha,date_format(hora, '%H:%i:%s') as hora from tbl_venta"
+        sql="select id,date_format(fecha, '%d-%m-%Y') as fecha,date_format(hora, '%H:%i:%s') as hora, total, tbl_user.nombre, tbl_user.apellido from tbl_venta inner join tbl_user on tbl_venta.id_user = tbl_user.id"
         try:
             self.cursor.execute(sql)
             columns = [column[0] for column in self.cursor.description]
@@ -155,7 +167,7 @@ class database:
             raise
 
     def select_venta_fechas(self,f1,f2):
-        sql="select id,date_format(fecha, '%d-%m-%Y') as fecha,date_format(hora, '%H:%i:%s') as hora, total from tbl_venta where tbl_venta.fecha between '{}' and '{}'".format(f1,f2)
+        sql="select id,date_format(fecha, '%d-%m-%Y') as fecha,date_format(hora, '%H:%i:%s') as hora, total, tbl_user.nombre, tbl_user.apellido from tbl_venta inner join tbl_user on tbl_venta.id_user = tbl_user.id where tbl_venta.fecha between '{}' and '{}'".format(f1,f2)
         try:
             self.cursor.execute(sql)
             columns = [column[0] for column in self.cursor.description]
@@ -167,7 +179,7 @@ class database:
             raise
 
     def select_venta_hora(self,f1,h1,h2):
-        sql="select id,date_format(fecha, '%d-%m-%Y') as fecha,date_format(hora, '%H:%i:%s') as hora from tbl_venta where tbl_venta.fecha = '{}' and tbl_venta.hora between '{}' and '{}'".format(f1,h1,h2)
+        sql="select id,date_format(fecha, '%d-%m-%Y') as fecha,date_format(hora, '%H:%i:%s') as hora, total, tbl_user.nombre, tbl_user.apellido from tbl_venta inner join tbl_user on tbl_venta.id_user = tbl_user.id where tbl_venta.fecha = '{}' and tbl_venta.hora between '{}' and '{}'".format(f1,h1,h2)
         try:
             self.cursor.execute(sql)
             columns = [column[0] for column in self.cursor.description]
@@ -298,9 +310,11 @@ def insert_u():
     name=request.form['username']
     password=request.form['password']
     rol=request.form['user_rol']
+    nombre=request.form['nombre']
+    apellido=request.form['apellido']
     password=password.encode('utf-8')
     hashed=bcrypt.hashpw(password,bcrypt.gensalt())
-    data=mydata.insert_user(name,hashed,rol)
+    data=mydata.insert_user(name,hashed,rol,nombre,apellido)
     return jsonify(data)
 #rutas de la tabla ropa
 @app.route("/list")
@@ -312,14 +326,28 @@ def listar():
     else:
         return jsonify({'Alert':'Invalid Token'})
 
+@app.route("/browser_ropa",methods=['POST'])
+def browser_name():
+    a=auth_in()
+    if a==True:
+        t=request.form['texto']
+        if t=='':
+            return jsonify({'err':'invalid format'})
+        else:
+            data=mydata.select_browser(t)
+            return data
+    else:
+        return jsonify({'Alert':'Invalid Token'})
+
 @app.route("/newropa", methods=['POST'])
 def insert():
     a=auth_in()
     if a==True:
         name=request.form['nombre']
         cant=request.form['cantidad']
+        talla=request.form['talla']
 
-        data=mydata.insert_ropa(name,cant)
+        data=mydata.insert_ropa(name,cant,talla)
         
         data=mydata.select_ropa(data)
 
@@ -390,7 +418,9 @@ def insert_product_venta():
         id_r=request.form['id_ropa']
         cantidad=request.form['cantidad']
         monto=request.form['monto']
-        data=mydata.insert_producto_venta(id_v,id_r,cantidad,monto)
+        user=request.form['user']
+        user_caja=request.form['user_caja']
+        data=mydata.insert_producto_venta(id_v,id_r,cantidad,monto,user,user_caja)
         data=mydata.select_venta_ropa(data)
 
         return jsonify(data)
